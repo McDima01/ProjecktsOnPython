01e341425b99571be71b1680a7c2b35efa426bfe
McDima01 <mcdima0001@gmail.com>
2025-04-09 21:49:08 +0300

    VektorBot

---
+++ VektorBot/bot.py
@@ -0,0 +1,30 @@
+import telebot
+import requests
+from config import *
+from handlers import register_handlers
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+register_handlers(bot)  # Регистрируем обработчики команд
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/setMyCommands"
+
+commands = [
+    {"command": "start", "description": "Перезапустить бота"},
+    {"command": "help", "description": "Помощь"},
+    {"command": "info", "description": "Информация о подъемниках"},
+    {"command": "menu", "description": "Главное меню"},
+    {"command": "contact", "description": "Связаться с нами"},
+    {"command": "rent", "description": "Аренда оборудования"},
+    {"command": "feedback", "description": "Посмотреть отзывы"},
+]
+
+response = requests.post(url, json={"commands": commands})
+print(response.json())
+
+if __name__ == "__main__":
+    print("Бот запущен")
+    bot.infinity_polling()
+
+
+# timeout=10, long_polling_timeout=5
